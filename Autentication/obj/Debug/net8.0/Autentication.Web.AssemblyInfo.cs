f473adf3dc368510261999374e3b6104065f3ca7
Nestor Villca  Mollisaca <105319566+Nestor147@users.noreply.github.com>
2025-09-08 15:51:34 -0400

    fix

--- Autentication/obj/Debug/net8.0/Autentication.Web.AssemblyInfo.cs
+++ Autentication/obj/Debug/net8.0/Autentication.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autentication.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ede3471729fc7cdd83ee04d124853183f7534a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d37a4bf39475430df1b94d3d536d156727f8cc7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autentication.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autentication.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
